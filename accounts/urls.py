4f381b599bce66b3449c46749f21fb750d33085f
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-09 22:19:15 +0100

    Fixed login next page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ import django.contrib.auth.views
 
 
 urlpatterns = [
-    url(r'^login/$', django.contrib.auth.views.login, name='login', kwargs={'next_page': '/'}),
+    url(r'^login/$', django.contrib.auth.views.login, name='login'),
     url(r'^logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^register/$', views.register, name='register'),
 ]
