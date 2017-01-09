1df5984e934f28fcffa21a764d2e8ca240fef26e
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-09 18:38:05 +0100

    Added LyricViewSet to API.

--- lyrics/api.py
+++ lyrics/api.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from lyrics.views import ArtistViewSet, AlbumViewSet, SongViewSet
+from lyrics.views import ArtistViewSet, AlbumViewSet, SongViewSet, LyricViewSet
 
 router = DefaultRouter()
 router.register('artists', ArtistViewSet)
 router.register('albums', AlbumViewSet)
 router.register('songs', SongViewSet)
+router.register('lyrics', LyricViewSet)
 
 urlpatterns = [
     url(r'', include(router.urls)),
