03909ffb8193fa2b56ebc20d796c9099c72af977
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-10 00:02:05 +0100

    Fixed CSRF error.

--- mysite/static/js/dialog.js
+++ mysite/static/js/dialog.js
@@ -6,14 +6,12 @@ $(document).ready(function() {
         $("#dialog").dialog("open");
     });
     $('#submit').on("click", function(e) {
-        var data = {};
         var form = $('#dialog').find('form');
-        form.serializeArray().map(function(x){data[x.name] = x.value;});
-        var json = JSON.stringify(data);
+        var data = form.serialize();
         var url = form.attr('action');
         $.post({
             url: url,
-            data: json,
+            data: data,
             dataType: "json",
             success: function(response) {
                 console.log(response);
