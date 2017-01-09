bfe4bb25b31e2a1f3c1a1d609fa0b39cb51b4981
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-09 23:53:45 +0100

    Now?

--- mysite/static/js/dialog.js
+++ mysite/static/js/dialog.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
         $("#dialog").dialog("open");
     });
     $('#submit').on("click", function(e) {
-        var data = $('#dialog').find('form').serialize();
+        var data = {};
+        $("#dialog").find('form').serializeArray().map(function(x){data[x.name] = x.value;});
         var json = JSON.stringify(data);
         console.log(json);
     })
