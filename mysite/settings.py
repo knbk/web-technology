acb203f3543f9da82e6131e5dd3132e61001703f
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-09 21:19:15 +0100

    Removed rest-swagger.

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_docs',
-    'rest_framework_swagger',
 
     'accounts.apps.AccountsConfig',
     'lyrics.apps.LyricsConfig',
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework_swagger.views import get_swagger_view
 
-
-schema_view = get_swagger_view(title='Lyrics API')
 
 urlpatterns = [
     url(r'', include('lyrics.urls')),
     url(r'^api/', include('lyrics.api')),
-    url(r'^api-docs/$', schema_view, name='docs'),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/docs/', include('rest_framework_docs.urls')),
     url(r'^admin/', admin.site.urls),
