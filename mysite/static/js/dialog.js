0c1222cdb0bef4ad17901be394a2cc3e60858e9e
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-09 23:39:09 +0100

    Updated js.

--- mysite/static/js/dialog.js
+++ mysite/static/js/dialog.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
     $('#dialog').dialog({
         autoOpen: false
     });
-
+    $('#open-dialog').on("click", function() {
+        $("#dialog").dialog("open");
+    });
+    $('#submit').on("click", function(e) {
+        data = $('#dialog').find('form');
+    })
 });
