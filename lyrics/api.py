a7d45213afd51fbf58e0349be6ce1e5922afef12
Marten Kenbeek <marten.knbk@gmail.com>
2017-01-09 17:39:06 +0100

    Added Album/Song viewsets.

--- lyrics/api.py
+++ lyrics/api.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from lyrics.views import ArtistViewSet
+from lyrics.views import ArtistViewSet, AlbumViewSet, SongViewSet
 
 router = DefaultRouter()
 router.register('artists', ArtistViewSet)
+router.register('albums', AlbumViewSet)
+router.register('songs', SongViewSet)
 
 app_name = 'api'
 urlpatterns = [
--- lyrics/views.py
+++ lyrics/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
-from lyrics.models import Artist
-from lyrics.serializers import ArtistSerializer
+from lyrics.models import Artist, Album, Song
+from lyrics.serializers import ArtistSerializer, AlbumSerializer, SongSerializer
 
 
 def home(request):
     return render(request, 'home.html')
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'artists': reverse('artist-list', request=request, format=format),
-    })
-
-
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
