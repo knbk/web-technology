a07f027ab3094e80355b09dbb1e3dd93bca367b3
Marten Kenbeek <marten.knbk@gmail.com>
2017-11-14 17:34:55 +0100

    Added Song and LyricRevision to admin.

--- lyrics/admin.py
+++ lyrics/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Artist, Album
+from .models import Artist, Album, Song, LyricRevision
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(LyricRevision)
